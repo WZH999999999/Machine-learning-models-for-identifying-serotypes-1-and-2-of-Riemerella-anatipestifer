d3b40dfe4774f98b985671e0596bc6ca771dc92e
WZH999999999 <89192327+WZH999999999@users.noreply.github.com>
2022-02-08 12:16:22 +0800

    Create Model building using Support Vector Machine algorithm.py

---
+++ Model building using Support Vector Machine algorithm.py
@@ -0,0 +1,45 @@
+#The first generation model for idendnetifing serotype 1 and 2 among Riemerella anatipestifer using Support Vector Machine algorithm
+#Data import
+import numpy as np
+import pandas as pd
+import os
+path=r"Path/to/the/file/for/machine/learning"
+files=os.listdir(path)
+data_list=[]
+for file in files:
+  file='\\'+file
+  tmp=pd.read_csv(path+file)
+  data=tmp.values
+  data=list(map(list,zip(*data)))
+  data=pd.DataFrame(data)
+  data.columns=data.loc[0,:]
+  data=data.drop([0])
+  data['strain']=file[1:-4]
+  if file[1:3]=='1-': #Make changes according to the label
+    data['label']='1'
+  else:
+    data['label']='0'
+  data_list.append(data)
+all_data = pd.concat(data_list)
+all_data.set_index('strain',inplace=True)
+all_data=all_data.fillna(0)
+#Support Vector Machine
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVC 
+from sklearn import metrics
+from time import time
+import datetime
+x=all_data.drop(['label'],axis=1)
+y=all_data['label']
+train_x,test_x,train_y,test_y=train_test_split(x,y,test_size=0.3,random_state=170)
+from sklearn.preprocessing import StandardScaler #Data standardization
+scaler = StandardScaler()
+Xtrain = scaler.fit_transform(train_x)
+Xtest = scaler.transform(test_x)
+Kernel=['linear','poly','rbf','sigmoid']
+for kernel in Kernel:
+  time0=time()
+  clf=SVC(kernel=kernel,gamma='auto',degree=5,cache_size=5000).fit(train_x,train_y)
+  print('The accuracy under kernel %s is %f' % (kernel,clf.score(test_x,test_y)))
+  print(datetime.datetime.fromtimestamp(time()-time0).strftime('%M:%S:%f')) #View the performance of the classifier under different kernel functions
