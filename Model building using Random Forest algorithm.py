5c4da3aa6e5ad257532a204fb236bd2326090865
WZH999999999 <89192327+WZH999999999@users.noreply.github.com>
2022-11-29 18:15:06 +0800

    Update Model building using Random Forest algorithm.py

--- Model building using Random Forest algorithm.py
+++ Model building using Random Forest algorithm.py
@@ -1,10 +1,10 @@
-#The model for idendnetifing serotype 1 and 2 among Riemerella anatipestifer using Random Forest algorithm
+#The model for idendnetifing serotypes 1 and 2 of Riemerella anatipestifer using Random Forest algorithm
 import numpy as np
 import pandas as pd
 import os
 path=r"Path/to/the/file/for/machine/learning"
 files=os.listdir(path)
-data_list=[]
+data_wzhlist=[]
 for file in files:
   file='\\'+file
   tmp=pd.read_csv(path+file)
@@ -18,11 +18,10 @@ for file in files:
     data['label']='1'
   else:
     data['label']='0'
-  data_list.append(data)
-all_data = pd.concat(data_list)
+  data_wzhlist.append(data)
+all_data = pd.concat(data_wzhlist)
 all_data.set_index('strain',inplace=True)
 all_data=all_data.fillna(0)
-#Random Forest Classifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier 
@@ -30,7 +29,7 @@ from sklearn import metrics
 x=all_data.drop(['label'],axis=1)
 y=all_data['label']
 train_x,test_x,train_y,test_y=train_test_split(x,y,test_size=0.3,random_state=150)
-rfc=RandomForestClassifier(random_state=150)
-model_one=rfc.fit(train_x,train_y)
+rfcc=RandomForestClassifier(random_state=150)
+model_one=rfcc.fit(train_x,train_y)
 prediction=model_one.predict(test_x)
 metrics.accuracy_score(prediction,test_y)
